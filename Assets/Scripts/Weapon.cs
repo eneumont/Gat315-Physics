db1843cbd6d66870107b1b91d6ecc35f5117229d
eneumont <129240077+eneumont@users.noreply.github.com>
2024-04-04 02:23:03 -0600

    fixed joint
    added fixed joint example for class

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,13 +6,21 @@ public class Weapon : MonoBehaviour {
     [SerializeField] GameObject[] ammo;
     [SerializeField] Transform emission;
     [SerializeField] AudioSource audio;
+	[SerializeField] float fireRate;
 
-    int toggle = 0;
+	int toggle = 0;
+	bool fireReady = true;
 
-    void Update() {
-        if (Input.GetMouseButtonDown(0)) {
-            audio.Play();
-            Instantiate(ammo[toggle], emission.position, emission.rotation);
-        }
-    }
+	void Update() {
+		if (fireReady && Input.GetMouseButtonDown(0)) {
+			Instantiate(ammo[toggle], emission.position, emission.rotation);
+			fireReady = false;
+			StartCoroutine(FireTimer(fireRate));
+		}
+	}
+
+	IEnumerator FireTimer(float time) {
+		yield return new WaitForSeconds(time);
+		fireReady = true;
+	}
 }
